118e2db5cf6902bfbaa16892e4c7afd0401bfad4
aezolotorev <aezolotorev@mail.ru>
2020-10-14 14:54:44 +0300

    HW3

---
+++ Assets/Scripts/MenuinGame.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuinGame : MonoBehaviour
+{
+    string namescene;
+    enum Screen
+    {
+        None,
+        Menu,
+       
+    }
+
+    public CanvasGroup menuScreen;
+
+    private void Start()
+    {
+        SetCurrentScreen(Screen.None);
+        namescene = SceneManager.GetActiveScene().name; 
+    }
+
+    void SetCurrentScreen(Screen screen)
+    {
+        Utility.SetCanvasGroupEnabled(menuScreen, screen == Screen.Menu);
+    }
+
+    // Start is called before the first frame update
+    public void OpenMenu()
+    {
+        SetCurrentScreen(Screen.Menu);
+        Time.timeScale = 0;
+    }
+
+    public void MainMenu()
+    {
+        SetCurrentScreen(Screen.None);
+        LoadingScreen.instance.LoadScene("MainMenu");
+        Time.timeScale = 1;
+    }
+
+    public void Reset()
+    {
+        
+        LoadingScreen.instance.LoadScene(namescene);
+        Time.timeScale = 1;
+    }
+
+    public void CloseMenu()
+    {
+        SetCurrentScreen(Screen.None);
+        Time.timeScale = 1;
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
